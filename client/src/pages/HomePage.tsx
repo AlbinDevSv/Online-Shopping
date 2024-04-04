300f9406fd2686f8088bdd6df8497a68700cbaf5
Albin <Albin.Svensson@medieinstitutet.se>
2024-04-04 22:01:22 +0200

    Added clientside

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <main>
+            <button style={{ width: "200px", height: "100px" }}>
+                Checkout
+            </button>
+        </main>
+    );
+};
+
+export default HomePage;
